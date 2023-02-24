34941dbc1a46aead4a95df0706391076ec531aa2
Martin Johnsson <sorill@gmail.com>
2023-02-24 15:43:28 +0100

    Documentation of data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Cattle genome table
+#'
+#' Physical and genetic length of cattle chromosomes
+#'
+#' @format A data frame with three columns: the name of each chromosome,
+#' the physical length in bp and the genetic length in centimorgans.
+#' @source ARS-UCD1.2 genome assembly and Ma et al. (2015)
+"cattle_genome_table"
