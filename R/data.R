31282480c597ca4b35bef1d9cba8202a9fcb4697
Martin Johnsson <sorill@gmail.com>
2023-12-13 14:53:45 +0100

    Addpig genome table

--- R/data.R
+++ R/data.R
@@ -6,3 +6,13 @@
 #' the physical length in bp and the genetic length in centimorgans.
 #' @source ARS-UCD1.2 genome assembly and Ma et al. (2015)
 "cattle_genome_table"
+
+
+#' Pig genome table
+#'
+#' Physical and genetic length of cattle chromosomes
+#'
+#' @format A data frame with three columns: the name of each chromosome,
+#' the physical length in bp and the genetic length in centimorgans.
+#' @source Sscrofa11.1 genome assembly and Tortereau et al. (2012)
+"pig_genome_table"
--- data-raw/genome_tables.R
+++ data-raw/genome_tables.R
@@ -5,3 +5,8 @@ cattle_genome_table <- readr::read_tsv("inst/extdata/cattle_genome_table.txt")
 cattle_genome_table <- cattle_genome_table[, c("chr", "length", "genetic_length")]
 
 usethis::use_data(cattle_genome_table, overwrite = TRUE)
+
+
+pig_genome_table <- readr::read_tsv("inst/extdata/pig_genome_table.txt")
+
+usethis::use_data(pig_genome_table, overwrite = TRUE)
